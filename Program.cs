ad32601258933d00a4f9ba7be7a601a8c89bf5c3
Lorenzo Fiore <lorenzo@efebia.com>
2019-09-26 22:17:46 +0200

    Complete Es1 and Es2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cryptopals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exectuting Es1...");
+            Es1.Exectute();
+
+            Console.WriteLine("Exectuting Es2...");
+            Es2.Exectute();
+        }
+    }
+}
